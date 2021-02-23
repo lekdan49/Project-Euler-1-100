9638cccef386db79eb72b780bc35910599b3fea5
lekdan49 <14988063+lekdan49@users.noreply.github.com>
2021-02-23 15:19:52 +1100

    Create 5.cpp

---
+++ 5/5.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <chrono>
+
+
+__int64 euler_5(const __int64& prev, const __int64& val)
+{
+
+	for (__int64 x = prev; x <= val; ++x)
+	{
+		for (__int64 i = 1; i <= 20; )
+		{
+			if ((x % i) == 0)
+			{
+				if (i == 20)
+				{
+					return x;
+				}
+				++i;
+			}
+			else
+			{
+				break;
+			}
+		}
+	}
+
+	euler_5(val, val + val);
+}
+
+int main()
+{
+	std::cout << euler_5(1, 1) << '\n';
+	return 0;
+}
